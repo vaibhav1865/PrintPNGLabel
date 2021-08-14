0ffc141418d673555f5100390b43454d4188a1d2
Vaibhav Saxena <Vaibhav.1865@gmail.com>
2021-08-14 20:20:41 +0530

    Added Code & Env file

---
+++ PrintPNG.js
@@ -0,0 +1,33 @@
+
+require('dotenv').config();
+const EasyPost = require('@easypost/api');
+
+const api = new EasyPost(API_KEY);
+
+const shipment = new api.Shipment({
+  to_address: {
+    street1: '345 California St',
+    street2: '',
+    city: 'San Francisco',
+    state: 'CA',
+    zip: '94104',
+    phone: '+1 415-528-7555'
+  },
+  from_address: {
+    street1: '345 California St',
+    street2: '',
+    city: 'San Francisco',
+    state: 'CA',
+    zip: '94104',
+    phone: '+1 415-528-7555'
+  },
+  parcel: {
+    length: 8,
+    width: 5,
+    height: 5,
+    weight: 5
+  }
+});
+
+
+shipment.save().then(s => s.buy(shipment.lowestRate(['USPS'], ['First'])).then(s => console.log(s.postage_label.label_url)).catch(console.log))
